5c61d4a873a0657294d76efe335c7029cc381556
Joshua Tiras <jot@yelp.com>
2016-04-11 18:44:48 -0700

    Fix port type

--- src/server/networking.h
+++ src/server/networking.h
@@ -2,7 +2,7 @@
 #define _NETWORKING_H
 
 typedef void (*handler_t)(char*, size_t);
-int init_networking(int port, handler_t h_func);
+int init_networking(const char *port, handler_t h_func);
 void start_network_loop();
 
 #endif /* _NETWORKING_H */
